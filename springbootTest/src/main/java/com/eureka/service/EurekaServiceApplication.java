bb3db1b65aa077e97b88f96e53508f15f753754a
oddream <cuizheng@live.cn>
2017-03-29 09:30:14 +0800

    spring cloud eureka server app

---
+++ springbootTest/src/main/java/com/eureka/service/EurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.eureka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer         //开启eureka服务
+@SpringBootApplication      //springBoot注解,spring在springBoot基础之上来构建项目
+public class EurekaServiceApplication {
+
+    //spirng boot的标准入口
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
